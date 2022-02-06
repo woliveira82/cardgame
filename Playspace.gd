e00a17a94aa8178be1ddd37b4c967bd7c31688cd
woliveira82@gmail.com <woliveira82@gmail.com>
2022-02-06 18:58:48 -0300

    part 3: drawing cards animation

--- Playspace.gd
+++ Playspace.gd
@@ -9,20 +9,52 @@ onready var deck_size = PlayerHand.CardList.size()
 onready var centre_card_oval = get_viewport().size * Vector2(0.5, 1.25)
 onready var hor_rad = get_viewport().size.x * 0.45
 onready var ver_rad = get_viewport().size.y * 0.4
-var angle = deg2rad(90) -0.5
+var angle = 0
+var card_number = 0
+var hand_card_size = 0
+var card_spreed = 0.25
 var oval_angle_vector = Vector2()
-
+enum{
+	IN_HAND,
+	IN_PLAY,
+	IN_MOUSE,
+	HAND_FOCUS,
+	DRAW_TO_HAND,
+	REORGANISING
+}
 
 func draw_card():
+	angle = PI / 2 + card_spreed * (float(hand_card_size) / 2 - hand_card_size)
 	var new_card = BaseCard.instance()
 	card_selected = randi() % deck_size
 	new_card.card_name = PlayerHand.CardList[card_selected]
 	oval_angle_vector = Vector2(hor_rad * cos(angle), - ver_rad * sin(angle))
-	new_card.rect_position = centre_card_oval + oval_angle_vector - new_card.rect_size / 2
+	new_card.start_position = $Deck.position - card_size / 2
+	new_card.target_position = centre_card_oval + oval_angle_vector - card_size
+	new_card.start_rotation = 0
+	new_card.target_rotation = (90 - rad2deg(angle)) / 4
 	new_card.rect_scale *= card_size / new_card.rect_size
-	new_card.rect_rotation = (90 - rad2deg(angle)) / 4
+	new_card.state = DRAW_TO_HAND
+	card_number = 0
+	for Card in $Cards.get_children():
+		angle = PI / 2 + card_spreed * (float(hand_card_size) / 2 - card_number)
+		oval_angle_vector = Vector2(hor_rad * cos(angle), - ver_rad * sin(angle))
+		Card.target_position = centre_card_oval + oval_angle_vector - card_size
+		Card.start_rotation = Card.rect_rotation
+		Card.target_rotation = (90 - rad2deg(angle)) / 4
+		card_number += 1
+		if Card.state == IN_HAND:
+			Card.start_position = Card.rect_position
+			Card.state = REORGANISING
+		elif Card.state == DRAW_TO_HAND:
+			Card.start_position = Card.target_position - (
+				(Card.target_position - Card.rect_position) / (1 - Card.t)
+			)
+
 	$Cards.add_child(new_card)
 	PlayerHand.CardList.erase(PlayerHand.CardList[card_selected])
-	angle += 0.2
+	angle += 0.25
 	deck_size -= 1
+	hand_card_size += 1
+	card_number += 1
 	return deck_size
--- cards/BaseCard.gd
+++ cards/BaseCard.gd
@@ -7,6 +7,25 @@ onready var card_info = CardDatabase.DATA[CardDatabase.get(card_name)]
 onready var card_image = str(
 	"res://assets/cards/", card_info[0].to_lower(), "/", card_name, ".png"
 )
+var start_position = 0
+var target_position = 0
+var start_rotation = 0
+var target_rotation = 0
+var t = 0
+const DRAW_TIME = 0.8
+const ORGANISE_TIME = 0.4
+onready var original_scale = rect_scale.x
+
+
+enum{
+	IN_HAND,
+	IN_PLAY,
+	IN_MOUSE,
+	HAND_FOCUS,
+	DRAW_TO_HAND,
+	REORGANISING
+}
+var state = IN_HAND
 
 
 func _ready():
@@ -14,6 +33,7 @@ func _ready():
 	$Border.scale *= card_size / $Border.texture.get_size()
 	$Card.texture = load(card_image)
 	$Card.scale *= card_size / $Card.texture.get_size()
+	$CardBack.scale *= card_size / $CardBack.texture.get_size()
 	var attack = str(card_info[1])
 	var retaliation = str(card_info[2])
 	var health = str(card_info[3])
@@ -24,3 +44,39 @@ func _ready():
 	$Bars/SpecialText/Name/CenterContainer/Type.text = special_text
 	$Bars/BottomBar/Attack/CenterContainer/AnR.text = str(attack , " / ", retaliation)
 	$Bars/BottomBar/Health/CenterContainer/Health.text = health
+
+
+func _physics_process(delta):
+	match state:
+		IN_HAND:
+			pass
+		IN_PLAY:
+			pass
+		IN_MOUSE:
+			pass
+		HAND_FOCUS:
+			pass
+		DRAW_TO_HAND:
+			if t <= 1:
+				rect_position = start_position.linear_interpolate(target_position, t)
+				rect_rotation = start_rotation * (1 - t) + (target_rotation * t)
+				rect_scale.x = original_scale * abs(1 - 2 * t)
+				if $CardBack.visible:
+					if t >= 0.5:
+						$CardBack.visible = false
+				t += delta / float(DRAW_TIME)
+			else:
+				rect_position = target_position
+				rect_rotation = target_rotation
+				state = IN_HAND
+				t = 0
+		REORGANISING:
+			if t <= 1:
+				rect_position = start_position.linear_interpolate(target_position, t)
+				rect_rotation = start_rotation * (1 - t) + (target_rotation * t)
+				t += delta / float(ORGANISE_TIME)
+			else:
+				rect_position = target_position
+				rect_rotation = target_rotation
+				state = IN_HAND
+				t = 0
