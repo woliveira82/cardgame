79ee1a695f5439bd9f7844d1ddf6a26f28b8480c
woliveira82@gmail.com <woliveira82@gmail.com>
2022-02-06 22:10:24 -0300

    part 4: zoom in on cards

--- cards/BaseCard.gd
+++ cards/BaseCard.gd
@@ -14,8 +14,17 @@ var target_rotation = 0
 var t = 0
 const DRAW_TIME = 0.8
 const ORGANISE_TIME = 0.4
-onready var original_scale = rect_scale.x
-
+const ZOOM_TIME = 0.2
+const ZOOM_SIZE = 2
+onready var original_scale = rect_scale
+var setup = true
+var start_scale = Vector2()
+var card_position = Vector2()
+var reorganise_neighbors = true
+var hand_card_size = 0
+var card_number = 0
+var NeighborCard
+var move_neighbor_check = false
 
 enum{
 	IN_HAND,
@@ -34,6 +43,8 @@ func _ready():
 	$Card.texture = load(card_image)
 	$Card.scale *= card_size / $Card.texture.get_size()
 	$CardBack.scale *= card_size / $CardBack.texture.get_size()
+	$Focus.rect_scale *= card_size / $Card.texture.get_size()
+	
 	var attack = str(card_info[1])
 	var retaliation = str(card_info[2])
 	var health = str(card_info[3])
@@ -55,12 +66,35 @@ func _physics_process(delta):
 		IN_MOUSE:
 			pass
 		HAND_FOCUS:
-			pass
-		DRAW_TO_HAND:
+			if setup:
+				reset()
 			if t <= 1:
 				rect_position = start_position.linear_interpolate(target_position, t)
-				rect_rotation = start_rotation * (1 - t) + (target_rotation * t)
-				rect_scale.x = original_scale * abs(1 - 2 * t)
+				rect_rotation = start_rotation * (1 - t) + 0 * t
+				rect_scale = start_scale * (1 - t) + original_scale * t * ZOOM_SIZE
+				t += delta / float(ZOOM_TIME)
+				if reorganise_neighbors:
+					reorganise_neighbors = false
+					hand_card_size = $"../../".hand_card_size - 1
+					if card_number - 1 >= 0:
+						move_neighbor_card(card_number - 1, true, 1.0)
+					if card_number - 2 >= 0:
+						move_neighbor_card(card_number - 2, true, 0.25)
+					if card_number + 1 <= hand_card_size:
+						move_neighbor_card(card_number + 1, false, 1.0)
+					if card_number + 2 <= hand_card_size:
+						move_neighbor_card(card_number + 2, false, 0.25)
+			else:
+				rect_position = target_position
+				rect_rotation = 0
+				rect_scale = original_scale * ZOOM_SIZE
+		DRAW_TO_HAND:
+			if setup:
+				reset()
+			if t <= 1:
+				rect_position = start_position.linear_interpolate(target_position, t)
+				rect_rotation = start_rotation * (1 - t) + target_rotation * t
+				rect_scale.x = original_scale.x * abs(2 * t - 1)
 				if $CardBack.visible:
 					if t >= 0.5:
 						$CardBack.visible = false
@@ -71,12 +105,73 @@ func _physics_process(delta):
 				state = IN_HAND
 				t = 0
 		REORGANISING:
+			if setup:
+				reset()
 			if t <= 1:
+				if move_neighbor_check:
+					move_neighbor_check = false
 				rect_position = start_position.linear_interpolate(target_position, t)
-				rect_rotation = start_rotation * (1 - t) + (target_rotation * t)
+				rect_rotation = start_rotation * (1 - t) + target_rotation * t
+				rect_scale = start_scale * (1 - t) + original_scale * t
 				t += delta / float(ORGANISE_TIME)
+				if reorganise_neighbors == false:
+					reorganise_neighbors = true
+					if card_number - 1 >= 0:
+						reset_cards_position(card_number - 1)
+					if card_number - 2 >= 0:
+						reset_cards_position(card_number - 2)
+					if card_number + 1 <= hand_card_size:
+						reset_cards_position(card_number + 1)
+					if card_number + 2 <= hand_card_size:
+						reset_cards_position(card_number + 2)
 			else:
 				rect_position = target_position
 				rect_rotation = target_rotation
+				rect_scale = original_scale
 				state = IN_HAND
-				t = 0
+
+
+func move_neighbor_card(card_index, left, spread_factor):
+	NeighborCard = $"../".get_child(card_index)
+	if left:
+		NeighborCard.target_position = NeighborCard.card_position - spread_factor * Vector2(65, 0)
+	else:
+		NeighborCard.target_position = NeighborCard.card_position + spread_factor * Vector2(65, 0)
+	NeighborCard.setup = true
+	NeighborCard.state = REORGANISING
+	NeighborCard.move_neighbor_check = true
+
+
+func reset_cards_position(card_index):
+	NeighborCard = $"../".get_child(card_index)
+	if NeighborCard.move_neighbor_check == false:
+		NeighborCard = $"../".get_child(card_index)
+		if NeighborCard.state != HAND_FOCUS:
+			NeighborCard.state = REORGANISING
+			NeighborCard.target_position = NeighborCard.card_position
+			NeighborCard.setup = true
+
+
+func reset():
+	start_position = rect_position
+	start_rotation = rect_rotation
+	start_scale = rect_scale
+	t = 0
+	setup = false
+
+
+func _on_Focus_mouse_entered():
+	match state:
+		IN_HAND, REORGANISING:
+			setup = true
+			target_position = card_position
+			target_position.y = get_viewport().size.y - $"../../".card_size.y * ZOOM_SIZE
+			state = HAND_FOCUS
+
+
+func _on_Focus_mouse_exited():
+	match state:
+		HAND_FOCUS:
+			setup = true
+			target_position = card_position
+			state = REORGANISING
